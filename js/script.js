1eff88ec3a0872731e2671f76d2adb10115a9e75
Jessica <jkustra92@gmail.com>
2016-05-10 12:18:19 -0400

    create css, js files. basic set up for what i need to get this game to work

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+console.log("hey, i'm working!")
+
+});
+
+function makeCards(){ //use a couple arrays and a forEach to add all the required object key/value pairs. 
+                      //also use images (sprites?) and add ids to them to get the image. give them all a class
+                      //to show the back of the card before it is flipped.
+                      //as an object is made, push it into the deck array. eeeyyyyy.
+};
+
+function shuffleCards(){ //use Durstenfeld shuffle!! :D
+
+};
+
+function startGame(){  //this could be where the player and dealer get their first cards?
+                      //pop elements from deck array and push it into a player/dealer array, respectively??
+}
+
+function checkTotal(){ //from my understanding, 
+
+}
+
+function hit(){       //pop off ONE array element from the deck. yes ma'am. also should make this generic and just
+                      //pass through the object so the player and dealer don't have two separate ones? just a thought! :D
+}
+
+function restartGame(){ //when restart button is clicked, do the thing.
+
+}
+
+function blackJack(){ //overall encompassing function? maybe?
+
+}
